cf42f299537a71390bd8b2797472cf450dd808db
Mxtsouko <99751673+Mxtsouko-off@users.noreply.github.com>
2024-09-09 19:51:08 +0200

    Update quest.py

--- cogs/quest.py
+++ cogs/quest.py
@@ -9,7 +9,7 @@ class Quest(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.questions = []
-        self.load_questions.start()
+        self.load_questions.start()  # Démarrer la tâche pour charger les questions
 
     @tasks.loop(hours=5) 
     async def send_random_question(self):
@@ -23,35 +23,34 @@ class Quest(commands.Cog):
             except Exception as e:
                 print(f"Erreur lors de la purge des messages: {e}")
 
-            question = random.choice(self.questions)
-
-            embed = disnake.Embed(
-                title="Question du jour",
-                description=question,
-                color=0x00ff00
-            )
-            embed.add_field(name='Hésitez pas à répondre dans :', value='https://discord.com/channels/1251476405112537148/1269373203650973726')
-
-            await channel.send(content=role.mention, embed=embed)
+            if self.questions:  # Vérifier si des questions sont disponibles
+                question = random.choice(self.questions)
+                embed = disnake.Embed(
+                    title="Question du jour",
+                    description=question,
+                    color=0x00ff00
+                )
+                embed.add_field(name='Hésitez pas à répondre dans :', value='https://discord.com/channels/1251476405112537148/1269373203650973726')
+                await channel.send(content=role.mention, embed=embed)
 
     @send_random_question.before_loop
     async def before_send_random_question(self):
         await self.bot.wait_until_ready()
 
-    @tasks.loop(hours=5)
+    @tasks.loop(count=1)  # Charge les questions une seule fois
     async def load_questions(self):
         async with aiohttp.ClientSession() as session:
             async with session.get('https://raw.githubusercontent.com/Mxtsouko-off/Misaki/main/Json/question.json') as response:
                 if response.status == 200:
                     data = await response.json()
                     self.questions = [item['question'] for item in data]
+                    print(f"{len(self.questions)} questions chargées.")
                 else:
                     print(f"Erreur lors du chargement des questions: {response.status}")
 
     @load_questions.before_loop
     async def before_load_questions(self):
         await self.bot.wait_until_ready()
-        await self.load_questions()
 
 def setup(bot):
     bot.add_cog(Quest(bot))
