ebd411f176f7f6d9efe4dea1a328e602886eee5a
Mxtsouko <99751673+Mxtsouko-off@users.noreply.github.com>
2024-09-10 15:25:30 +0200

    Update Fivem.py

--- cogs/Fivem.py
+++ cogs/Fivem.py
@@ -3,6 +3,7 @@ from disnake.ext import commands
 import aiohttp
 import base64
 import io
+import json
 
 class Fivem(commands.Cog):
 
@@ -11,61 +12,26 @@ class Fivem(commands.Cog):
 
     @commands.command(name='fivemlookup', help="Recherche un serveur FiveM par son tag")
     async def fivemlookup(self, ctx, tag: str):
-        urlfivem = f"https://servers-frontend.fivem.net/api/servers/single/{tag}"
-        headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.3",
-        }
+            urlfivem = f"https://servers-frontend.fivem.net/api/servers/single/{tag}"
+            headers = {
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.3",
+            }
 
-        async with aiohttp.ClientSession() as session:
-            async with session.get(urlfivem, headers=headers) as res:
-                if res.status == 404:
-                    embed = disnake.Embed(
-                        title="Invalid Code",
-                        color=0xc73e10,
-                    )
-                    embed.set_author(name=ctx.author.display_name, icon_url=ctx.author.avatar.url)
-                    await ctx.send(embed=embed)
-                else:
-                    data = await res.json()
-                    connect_endpoint = data["Data"]["connectEndPoints"][0]
-
-                    if not connect_endpoint.startswith("http"):
-                        split = connect_endpoint.split(":")
-                        urlip = f"http://ip-api.com/json/{split[0]}"
-                        async with session.get(urlip) as res2:
-                            out2 = await res2.json()
-
-                            if "icon" in out2:
-                                icon = out2["icon"]
-                                file = disnake.File(io.BytesIO(base64.b64decode(icon)), filename="graph.png")
-                                embed = disnake.Embed(
-                                    color=0xc73e10,
-                                )
-                                embed.set_author(name=ctx.author.display_name, icon_url=ctx.author.avatar.url)
-                                embed.add_field(name="IP:Port", value=f"`{connect_endpoint}`")
-                                embed.add_field(name="Server Details", value=f"IP: `{split[0]}`\n Country: `{out2['country']}`\n City: `{out2['city']}`\n ISP: `{out2['isp']}`\n Org: `{out2['org']}`\n Zip Code: `{out2['zip']}`\n Timezone: `{out2['timezone']}`\n")
-                                embed.add_field(name="FiveM Server", value=f"Server Name: `{data['Data']['hostname'][:390]}`\n Online Players: `{len(data['Data']['players'])}`\n Max Players: `{data['Data']['svMaxclients']}`\n Artifacts: `{data['Data']['server']}`\n Resources: `{len(data['Data']['resources'])}`\n Onesync Enabled?: `{data['Data']['vars']['onesync_enabled']}`\n", inline=True)
-                                embed.set_footer(text="By Mxtsouko")
-                                embed.set_thumbnail(url="attachment://graph.png")
-                                await ctx.send(embed=embed, file=file)
-                            else:
-                                embed = disnake.Embed(
-                                    color=0xc73e10,
-                                )
-                                embed.set_author(name=ctx.author.display_name, icon_url=ctx.author.avatar.url)
-                                embed.add_field(name="IP:Port", value=f"`{connect_endpoint}`")
-                                embed.add_field(name="Server Details", value=f"IP: `{split[0]}`\n Country: `{out2['country']}`\n City: `{out2['city']}`\n ISP: `{out2['isp']}`\n Org: `{out2['org']}`\n Zip Code: `{out2['zip']}`\n Timezone: `{out2['timezone']}`\n")
-                                embed.add_field(name="FiveM Server", value=f"Server Name: `{data['Data']['hostname'][:390]}`\n Online Players: `{len(data['Data']['players'])}`\n Max Players: `{data['Data']['svMaxclients']}`\n Artifacts: `{data['Data']['server']}`\n Resources: `{len(data['Data']['resources'])}`\n Onesync Enabled?: `{data['Data']['vars']['onesync_enabled']}`\n", inline=True)
-                                embed.set_footer(text="By Mxtsouko")
-                                await ctx.send(embed=embed)
+            async with aiohttp.ClientSession() as session:
+                async with session.get(urlfivem, headers=headers) as res:
+                    if res.status == 404:
+                        await ctx.send("Le tag fourni est invalide.")
                     else:
-                        embed = disnake.Embed(
-                            title="Cannot find server details...",
-                            description=f"`{connect_endpoint}`",
-                            color=0xc73e10,
-                        )
-                        embed.set_author(name=ctx.author.display_name, icon_url=ctx.author.avatar.url)
-                        await ctx.send(embed=embed)
+                        data = await res.json()
+
+
+                        json_data = json.dumps(data, indent=4) 
+                        json_file = io.BytesIO(json_data.encode('utf-8'))   
+
+                        dm_channel = await ctx.author.create_dm()
+                        await dm_channel.send("Voici les informations du serveur FiveM que vous avez demandées.", file=disnake.File(fp=json_file, filename=f"fivem_data_{tag}.json"))
+                        
+                        await ctx.send(f"Les informations sur le serveur {tag} ont été envoyées en message privé.")
 
     @commands.command(name='cip', help="Obtenir l'IP et le port d'un serveur FiveM")
     async def cip(self, ctx, tag: str):
